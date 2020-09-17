ca088903f89fc6ca2cb858a77771e3c5b44c4e9c
Caronte Martina <m.caronte@reply.it>
2020-09-17 12:27:57 +0200

    draft website with bootstrap

--- bootstrap/animation.js
+++ bootstrap/animation.js
@@ -1,8 +1,10 @@
 function quotationText() {
   let quotes = {
     Aristotele: '"Le persone perfette non combattono, non mentono, non commettono errori e non esistono."',
-    Cicerone: '"La dignità non consiste nel possedere onori, ma nella consapevolezza di meritarli."',
-    Gesu: '"Se sulla terra prevalesse l\'amore, tutte le leggi sarebbero superflue."'
+    Mandela: '"The greatest glory in living lies not in never falling, but in rising every time we fall."',
+    WaltDisney: '"The way to get started is to quit talking and begin doing."',
+    SteveJobs: '"Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking."',
+    Roosevelt: '"If life were predictable it would cease to be life, and be without flavor."'    
   };
   let colors = [ "Tomato", "Orange", "DodgerBlue", "MediumSeaGreen", "Gray", "Violet"
               ];
@@ -13,5 +15,5 @@ function quotationText() {
 
   document.getElementById("text").innerHTML = text;
   document.getElementById("author").innerHTML = author;
-  document.getElementsByClassName("page-background").innerHTML = rcolor;
+  document.body.style.background = rcolor;
 }
